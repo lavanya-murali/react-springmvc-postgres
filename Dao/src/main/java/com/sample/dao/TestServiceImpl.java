7f33401f498820322293b8e22e7b9286648e1aec
Lavanya Murali <lavanyamurali85@gmail.com>
2017-04-28 11:52:26 +0530

    JPA Repository integration with Postgres local DB

--- Dao/src/main/java/com/sample/dao/TestServiceImpl.java
+++ Dao/src/main/java/com/sample/dao/TestServiceImpl.java
@@ -1,16 +1,20 @@
 package com.sample.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.sample.dao.entity.TestEntity;
+import com.sample.dao.entity.TestRepository;
 
 @Service
 public class TestServiceImpl implements TestService{
-//	@Autowired
-//	TestRepository testRepository;	
+	@Autowired
+	TestRepository testRepository;	
 	
 public String checkService(){
-//	TestEntity testEntity = new TestEntity();
-//	testEntity.setTestString("checking");
-//	testRepository.save(testEntity);
+	TestEntity testEntity = new TestEntity();
+	testEntity.setTestString("checking");
+	testRepository.save(testEntity);
 	return "Hello in Service";
 }
 }
---
+++ Dao/src/main/java/com/sample/dao/entity/TestEntity.java
@@ -0,0 +1,40 @@
+package com.sample.dao.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "test_table")
+public class TestEntity implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "test_id", unique = true)
+	private Long testId;
+	@Column(name = "test_string")
+	private String testString;
+	public Long getTestId() {
+		return testId;
+	}
+	public void setTestId(Long testId) {
+		this.testId = testId;
+	}
+	public String getTestString() {
+		return testString;
+	}
+	public void setTestString(String testString) {
+		this.testString = testString;
+	}
+	
+	
+}
